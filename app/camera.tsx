2371f53512e6db7272ff7fdeb135f676c4992ac0
HomeroKemmerich <hmvkemmerich@gmail.com>
2025-04-22 22:24:01 -0300

    feat: updates the camera page

--- app/camera.tsx
+++ app/camera.tsx
@@ -1,14 +1,16 @@
-import RenderCamera from "@/components/RenderCamera";
-import RenderPicture from "@/components/RenderPicture";
+import RenderCamera from "@/components/renderers/RenderCamera";
 import {
     CameraView,
     useCameraPermissions,
 } from "expo-camera";
+import * as MediaLibrary from 'expo-media-library';
+import { useRouter } from "expo-router";
 import { useRef, useState } from "react";
 import { Button, Text, View } from "react-native";
 import styles from "../constants/styles";
 
 export default function Camera() {
+    const router = useRouter()
     const [permission, requestPermission] = useCameraPermissions();
     const ref = useRef<CameraView>(null);
     const [uri, setUri] = useState<string | null>(null);
@@ -30,13 +32,17 @@ export default function Camera() {
 
     const takePicture = async () => {
         const photo = await ref.current?.takePictureAsync();
-        setUri(photo?.uri);
+
+        // Cria um asset para que a imagem seja armazenada
+        const asset = await MediaLibrary.createAssetAsync(photo!.uri);
+
+        router.push({ pathname: '/preview', params: { uri: asset.uri } });
     };
 
     return (
         <View style={styles.container}>
-            {/*TODO: Return compopnents, not functions */}
-            {uri ? RenderPicture(uri) : RenderCamera(ref, takePicture)}
+            {/*TODO: Return components, not functions */}
+            {RenderCamera(ref, takePicture)}
         </View>
     );
 }
